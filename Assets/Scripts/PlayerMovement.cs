1d7bcd294357bf9eb1769e5263f336ad11d7e081
Vova Hash <70117031+VovaHash@users.noreply.github.com>
2025-06-30 09:58:34 +0300

    Spine Animation setup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,16 @@
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float walkSpeed;
+    public GameObject playerVisual;
+    private Animator anim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        anim = playerVisual.GetComponent<Animator>();
         
     }
 
@@ -16,16 +20,19 @@ public class PlayerMovement : MonoBehaviour
 
         // Barebones movement, will delete/change later
 
+        anim.SetBool("Running", false);
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+            anim.SetBool("Running", true);
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector2.right * walkSpeed * Time.deltaTime);
+            anim.SetBool("Running", true);
         }
 
-        
+
     }
 }
