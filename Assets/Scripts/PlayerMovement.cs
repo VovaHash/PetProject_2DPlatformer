09e6b969a83960c790aaf068f4a7acb77b9272e6
Vova Hash <70117031+VovaHash@users.noreply.github.com>
2025-06-30 11:03:48 +0300

    Fixed Spine framerate issue

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float walkSpeed;
-    public GameObject playerVisual;
-    private Animator anim;
+    public GameObject playerVisualObj;
+    private Animator animatorParams;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        anim = playerVisual.GetComponent<Animator>();
+        animatorParams = playerVisualObj.GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        animatorParams.SetBool("Running", false);
         // Barebones movement, will delete/change later
 
-        anim.SetBool("Running", false);
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
-            anim.SetBool("Running", true);
+            animatorParams.SetBool("Running", true);
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector2.right * walkSpeed * Time.deltaTime);
-            anim.SetBool("Running", true);
+            animatorParams.SetBool("Running", true);
         }
 
 
