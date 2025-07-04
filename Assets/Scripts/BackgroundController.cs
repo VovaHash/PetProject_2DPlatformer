99d9bf9d761480e8d709b10a967a77d14ba4a5a3
Vova Hash <70117031+VovaHash@users.noreply.github.com>
2025-07-04 22:18:05 +0300

    infinite parallax

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -4,6 +4,7 @@ public class BackgroundController : MonoBehaviour
 {
     public GameObject camera;
     private float startPositionX;
+    private float length;
     public float parallaxEffect;
     private float distanceX;
     public float smoothingX;
@@ -12,14 +13,27 @@ public class BackgroundController : MonoBehaviour
     void Start()
     {
         startPositionX = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {   
         float distanceX = camera.transform.position.x * parallaxEffect;
+        float movement = camera.transform.position.x * (1 - parallaxEffect);
+
         Vector3 newPositionX = new Vector3(startPositionX + distanceX, transform.position.y, transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, newPositionX, smoothingX * Time.deltaTime);
+        //transform.position = Vector3.Lerp(transform.position, newPositionX, smoothingX * Time.deltaTime);
+        transform.position = newPositionX;
+
+        if (movement > startPositionX + length)
+        {
+            startPositionX += length;
+        }
+        else if (movement < startPositionX - length)
+        {
+            startPositionX -= length;
+        }
     }
 
 
